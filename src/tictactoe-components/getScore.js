a7186ad60f2e5802a1a9161bef01f7efb0f4b8a8
gun99-dev <bazooka9966@gmail.com>
2023-05-01 04:23:17 +0800

    Rename the function for better readability

--- src/tictactoe-components/getScore.js
+++ src/tictactoe-components/getScore.js
@@ -1,11 +1,13 @@
 
-export default function getScore(user) {
+export default function fetchScoreFromCookie(user) {
     let value = 0;
     for (let cookie of document.cookie.split(';')) {
         cookie = cookie.trim();
         if (user === cookie.substring(0, user.length)) {
-            value = cookie.substring(user.length + 2 , cookie.length);
+            value = cookie.substring(user.length + 1 , cookie.length);
         }
     }
+    value = parseInt(value)
+    value  = isNaN(value) ? 0 : value
     return value;
 }
